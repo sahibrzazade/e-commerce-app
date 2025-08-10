7c1acf0d5242555008ee8772256f3b235c2c5c05
sahibrzazade <sahibrzazade03@gmail.com>
2025-08-10 23:58:47 +0400

    feat(86euetrg2): add NotFound page for handling unknown routes

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import AppLayout from '../layouts/AppLayout';
+import { OutlinedButton } from '../components/OutlinedButton';
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+
+const NotFound: React.FC = () => {
+    const { t } = useTranslation();
+
+    const navigate = useNavigate();
+
+    return (
+        <AppLayout>
+            <div className="flex flex-col items-center justify-center min-h-screen text-center">
+                <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
+                <p className="text-lg mb-6">Sorry, the page you are looking for does not exist.</p>
+                <OutlinedButton content={t("common:go-back-to-home")} height={60} width={200} fontWeight='bold' onClick={() => navigate("/home")} />
+            </div>
+        </AppLayout>
+    );
+};
+
+export default NotFound;
